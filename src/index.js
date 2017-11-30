4c64d5109fe4c1808a170dafe92916ea5d20d75f
Dmitri Redcenco <dredcenco@gmail.com>
2017-11-30 12:51:29 -0500

    add main entry to a lib

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * from './is';
+export * from './create-storage';
+export * from './maps';
+export * from './mixin';
+export * from './objects';
+export * from './unique-id';
+export * from './symbols';
