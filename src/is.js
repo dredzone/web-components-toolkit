3b19777d4a048d4c7b12fac52a70aacc7ec6091c
Dmitri Redcenco <dredcenco@gmail.com>
2017-12-01 11:03:39 -0500

    add name spacing to avoid methods collisions

--- src/is.js
+++ src/is.js
@@ -3,4 +3,8 @@ import {isTypeChecks} from './is/is-type-checks';
 import {isArithmeticChecks} from './is/is-arithmetic-checks';
 import {isDomChecks} from './is/is-dom-checks';
 
-export const is = merge.all([isTypeChecks, isArithmeticChecks, isDomChecks]);
+export const is = merge.all([
+	{type: isTypeChecks},
+	{number: isArithmeticChecks},
+	{dom: isDomChecks}
+]);
