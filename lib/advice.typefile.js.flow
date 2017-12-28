7a306fbbef46345fbe7b7198554b00110a59015b
Dmitri Redcenco <dredcenco@gmail.com>
2017-12-28 11:13:12 -0500

    add advice class decorators

---
+++ lib/advice.typefile.js.flow
@@ -0,0 +1,10 @@
+/* @flow */
+export type AdviceType = {
+	before(behaviour: Function, ...methodNames: string[]): Function,
+
+	after(behaviour: Function, ...methodNames: string[]): Function,
+
+	around(behaviour: Function, ...methodNames: string[]): Function,
+
+	afterThrow(behaviour: Function, ...methodNames: string[]): Function
+};
---
+++ src/advice.decorators.js
@@ -0,0 +1,67 @@
+/* @flow */
+import type {AdviceType} from './advice.typefile';
+
+const {defineProperty} = Object;
+
+export const advice: AdviceType = Object.freeze({
+	before: (behaviour: Function, ...methodNames: string[]): Function =>
+		(clazz: Class<any>) => {
+			for (let methodName: string of methodNames) {
+				const method: Function = clazz.prototype[methodName];
+				defineProperty(clazz.prototype, methodName, {
+					value: function (...args) {
+						behaviour.apply(this, args);
+						return method.apply(this, args);
+					},
+					writable: true
+				});
+			}
+			return clazz;
+		},
+
+	after: (behaviour: Function, ...methodNames: string[]): Function =>
+		(clazz: Class<any>) => {
+			for (let methodName: string of methodNames) {
+				const method: Function = clazz.prototype[methodName];
+				defineProperty(clazz.prototype, methodName, {
+					value: function (...args) {
+						const returnValue: any = method.apply(this, args);
+						behaviour.apply(this, args);
+						return returnValue;
+					},
+					writable: true
+				});
+			}
+			return clazz;
+		},
+
+	around: (behaviour: Function, ...methodNames: string[]): Function =>
+		(clazz: Class<any>) => {
+			for (let methodName: string of methodNames) {
+				const method: Function = clazz.prototype[methodName];
+				defineProperty(clazz.prototype, methodName, {
+					value: behaviour(method),
+					writable: true
+				});
+			}
+			return clazz;
+		},
+
+	afterThrow: (behaviour: Function, ...methodNames: string[]): Function =>
+		(clazz: Class<any>) => {
+			for (let methodName: string of methodNames) {
+				const method: Function = clazz.prototype[methodName];
+				defineProperty(clazz.prototype, methodName, {
+					value: function (...args) {
+						try {
+							return method.apply(this, args);
+						} catch (err) {
+							behaviour.call(this, err);
+						}
+					},
+					writable: true
+				});
+			}
+			return clazz;
+		}
+});
