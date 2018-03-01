ab034a29ae70f1f6665b7e3d789cc5cce1e587b2
Dmitri Redcenco <dredcenco@gmail.com>
2018-02-28 20:00:25 -0500

    add flow type

--- src/element-children.utility.js
+++ src/element-children.utility.js
@@ -1,5 +1,6 @@
+/* @flow */
 export const elementChildren: Function = (element: Element, nodeType: number = 1): Array<Element> => {
-	let childNodes: Array<Node> = element.childNodes;
+	let childNodes: NodeList<any> = element.childNodes;
 	let children: Array<Element> = [];
 	if (childNodes && childNodes.length > 0) {
 		let i: number = childNodes.length;
