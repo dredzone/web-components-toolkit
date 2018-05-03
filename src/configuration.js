50e51d34a4166e0690b386d22dd9cdf401f3d130
Dmitri Redcenco <dredcenco@gmail.com>
2018-05-03 10:27:25 -0400

    change interface on object utils

--- src/configuration.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-export interface IConfiguration {
-	get(key: string): any;
-
-	set(key: string, value: any): void;
-}
--- src/object.js
+++ src/object.js
@@ -1,19 +1,19 @@
 /* @flow */
-import getNestedValue from './object/get-nested-value';
-import setNestedValue from './object/set-nested-value';
+import dget from './object/dget';
+import dset from './object/dset';
 import toMap from './object/to-map';
 
 export type ObjectUtil = {
-	get(obj: Object, key: string, defaultValue: any): any;
+	dget(obj: Object, key: string, defaultValue: any): any;
 
-	set(obj: Object, key: string, value: any): void;
+	dset(obj: Object, key: string, value: any): void;
 
 	toMap(o: Object): Map<any, any>;
 }
 
 const object: ObjectUtil = Object.freeze({
-	get: getNestedValue,
-	set: setNestedValue,
+	dget,
+	dset,
 	toMap
 });
 
