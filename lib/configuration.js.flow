c22153a26fee32adf27af3a336d67d5a65760cbd
Dmitri Redcenco <dredcenco@gmail.com>
2018-05-02 21:08:48 -0400

    add debounce, throttle and configuration

---
+++ lib/configuration.js.flow
@@ -0,0 +1,6 @@
+/* @flow */
+export interface IConfiguration {
+	get(key: string): any;
+
+	set(key: string, value: any): void;
+}
---
+++ src/debounce.js
@@ -0,0 +1,11 @@
+/* @flow */
+export default (callback: Function, delay: number): Function => {
+	let timer: ?number;
+	return function (...args: Array<any>) {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			timer = null;
+			callback(...args);
+		}, delay);
+	};
+};
