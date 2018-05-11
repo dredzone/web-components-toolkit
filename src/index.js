322a22a061f6df8b4fc1bd8789e21ff5d53c53c6
Dmitri Redcenco <dredcenco@gmail.com>
2018-05-11 14:20:24 -0400

    add event-hub to exports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 export { default as classBuilder } from './class-builder.js';
 export { default as createStorage } from './create-storage.js';
 export { default as type } from './type.js';
+export { default as eventHub } from './event-hub';
 export * from './advice.js';
 export * from './array.js';
 export * from './browser.js';
