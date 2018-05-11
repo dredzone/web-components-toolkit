3515291fecf1341d52d19335d72b0cf5e3288323
Dmitri Redcenco <dredcenco@gmail.com>
2018-05-11 09:26:11 -0400

    cleanup

--- test/browser/create-element.js
+++ test/browser/create-element.js
@@ -1,11 +1,11 @@
 import createElement from '../../lib/browser/create-element.js';
 
 describe('create-element', () => {
-	it('create element', () => {
-		const el = createElement(`
+  it('create element', () => {
+    const el = createElement(`
 			<div class="my-class">Hello World</div>
 		`);
-		expect(el.classList.contains('my-class')).to.equal(true);
-		assert.instanceOf(el, Node, 'element is instance of node');
-	});
+    expect(el.classList.contains('my-class')).to.equal(true);
+    assert.instanceOf(el, Node, 'element is instance of node');
+  });
 });
