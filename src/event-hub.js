5d6d8a66bcbb59e0da48473a840d0e3aba387a7b
Dmitri Redcenco <dredcenco@gmail.com>
2018-05-11 14:17:33 -0400

    event-hub for pub-sub messages exchange

---
+++ src/event-hub.js
@@ -0,0 +1,26 @@
+/* @flow */
+
+export type TEventHub = {
+  emit(event: string, data: any): void,
+
+  on(event: string, handler: Function): void,
+
+  off(event: string, handler: Function): void
+};
+
+const eventHub = (): TEventHub => ({
+  hub: Object.create(null),
+  emit(event: string, data: any): void {
+    (this.hub[event] || []).forEach(handler => handler(data));
+  },
+  on(event: string, handler: Function): void {
+    if (!this.hub[event]) this.hub[event] = [];
+    this.hub[event].push(handler);
+  },
+  off(event: string, handler: Function): void {
+    const i: number = (this.hub[event] || []).findIndex(h => h === handler);
+    if (i > -1) this.hub[event].splice(i, 1);
+  }
+});
+
+export default eventHub();
