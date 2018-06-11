9914b18f9d29487a2f19df3beb7719319460ee08
Dmitri Redcenco <dredcenco@gmail.com>
2018-06-10 22:29:15 -0400

    add createEventHub to exports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 export { default as classBuilder } from './class-builder.js';
 export { default as createStorage } from './create-storage.js';
 export { default as http } from './http';
+export { default as createEventHub } from './event-hub';
 export { default as is } from './type.js';
 export * from './web-components.js';
 export * from './dom.js';
